bf737e6c1d31f2842bd58c60315a4f11b41b1387
MridulChauhan <mridulchauhan1998@gmail.com>
2019-07-23 18:06:12 +0530

    Add files via upload

---
+++ PIR_Motion_Sensor.ino
@@ -0,0 +1,24 @@
+/*     Arduini PIR Motion Sensor Tutorial
+ *      
+ *  by Dejan Nedelkovski, www.HowToMechatronics.com
+ *  
+ */
+int pirSensor = 11;
+int relayInput = 7;
+void setup() {
+  pinMode(pirSensor, INPUT);
+  pinMode(relayInput, OUTPUT); 
+  Serial.begin(9600); 
+}
+void loop() {
+  int sensorValue = digitalRead(pirSensor);
+  if (sensorValue == 1) 
+  {
+    Serial.println(sensorValue);
+    digitalWrite(relayInput,HIGH); // The Relay Input works Inversly
+  }
+  else
+  {
+    digitalWrite(relayInput,LOW);
+  }
+}
